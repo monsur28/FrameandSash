caeb31aeb73a8237beb4e871a7cdf75d5c0bbc44
monsur28 <monsur286512@gmail.com>
2025-01-20 11:27:01 +0600

    add

--- src/Hooks/AsiosSecure.jsx
+++ src/Hooks/AsiosSecure.jsx
@@ -3,7 +3,6 @@ import Cookies from "js-cookie"; // Ensure you install the js-cookie library: `n
 
 const axiosSecure = axios.create({
   baseURL: "http://frameandsash.great-site.net/api", // Base URL for your API
-  withCredentials: true, // Include cookies in the request
 });
 
 axiosSecure.interceptors.request.use(
